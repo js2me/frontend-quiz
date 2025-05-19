8644568c54bf604c1c93e66a2830ec488349a406
Sergey Volkov <js2me@outlook.com>
2025-05-19 23:30:16 +0300

    chore: add drop shadow for tyson

--- src/pages/_layout/ui/container.tsx
+++ src/pages/_layout/ui/container.tsx
@@ -35,7 +35,7 @@ export const Container = observer(({ children }: { children?: ReactNode }) => {
         src={tysonImage}
         alt={''}
         style={{
-          filter: 'drop-shadow(0px 2px 7px #00000086)',
+          filter: 'drop-shadow(rgba(0, 0, 0, 0.225) 0px 2px 7px)',
         }}
         className={
           'fixed bottom-2 right-2 w-36 -scale-x-100 dark:brightness-75 active:-scale-x-105 active:scale-y-105 cursor-pointer select-none'
